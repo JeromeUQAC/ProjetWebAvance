ffc4ac53b378ca41e5850f958418ddb5bfe14d39
JeromeUQAC <xer3qx157.jeux@gmail.com>
2024-03-04 19:33:14 -0500

    commit html + css initial

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import requests
+import json
 from flask import Flask
-
 app = Flask(__name__)
 
+
+@app.route('/shops/products/')
+def get_products():
+    return requests.get('/http://dimprojetu.uqac.ca/~jgnault/shops/products/').content
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -10,3 +15,5 @@ def hello_world():  # put application's code here
 
 if __name__ == '__main__':
     app.run()
+    products = json.dumps(get_products())
+    print(products)
