f3992ab62079b95ae6ab79ae29b07e90d4347467
JeromeUQAC <xer3qx157.jeux@gmail.com>
2024-03-05 10:25:42 -0500

    Fonction get products fonctionnelle

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import requests
 import json
-from flask import Flask
+
+from flask import Flask, render_template, request, redirect
+from urllib.request import urlopen
+
 app = Flask(__name__)
 
 
-@app.route('/shops/products/')
-def get_products():
-    return requests.get('/http://dimprojetu.uqac.ca/~jgnault/shops/products/').content
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/home')
+def home():  # put application's code here
+    return render_template("index.html")
+
+
+@app.route("/result", methods=["GET"])
+def get_products():
+    with urlopen("http://dimprojetu.uqac.ca/~jgnault/shops/products/") as response:
+        products = response.read()
+
+    return render_template("index.html", products=products)
 
 
 if __name__ == '__main__':
-    app.run()
-    products = json.dumps(get_products())
-    print(products)
+    app.run(debug=True, port=5000)
